f0482e37a73b31e583ad317d236ca7d2bda542fa
Sravanthi931 <ksravs917@gmail.com>
2025-05-24 20:50:13 +0530

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
       <Link to="/app4">App4</Link>-
       <Link to="/app5">App5</Link>-
       <Link to="/app6">App6</Link>-
-      <Link to="/app7">App6</Link>
+      <Link to="/app7">App7</Link>
     </div>
   );
 }
