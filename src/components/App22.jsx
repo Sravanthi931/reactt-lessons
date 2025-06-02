b86fc24b7ca38e865ddbcf6954fd7c40b6ebdeb8
Sravanthi931 <ksravs917@gmail.com>
2025-06-02 12:59:00 +0530

    up

--- src/components/App22.jsx
+++ src/components/App22.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+export default function App22() {
+  const [tmp, setTmp] = useState();
 
-export default function App22(){
-    return (
-        <div>App22</div>
-    )
-      
-    
+  const fetchWeather = async () => {
+    const res = await axios.get("http://localhost:8080/weather");
+    setTmp(res.data);
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+  return <div style={{backgroundColor:'pink'}}>{tmp}</div>;
 }
